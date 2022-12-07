150759066e4a7e322f3da9778044dcc302b86313
CoreDX1 <chrismaquis@gmail.com>
2022-12-07 18:28:09 -0300

    :sparkles: Agregando login
    Ahora se tiene que enviar a la DB para saber si existe un usuario

--- ClientService/Controllers/UsuarioController.cs
+++ ClientService/Controllers/UsuarioController.cs
@@ -12,49 +12,62 @@ namespace ClientService.Controllers;
 [Route("api/[Controller]")]
 public class UsuarioController : Controller
 {
-  private IUsuario<Usuario> usuario;
-  public IConfiguration _configuration;
+    private IUsuario<Usuario> usuario;
+    public IConfiguration _configuration;
 
-  public UsuarioController(IUsuario<Usuario> _usuario, IConfiguration configuration)
-  {
-    this.usuario = _usuario;
-    this._configuration = configuration;
-  }
-
-  [HttpGet]
-  public async Task<IActionResult> Get()
-  {
-    var data = await usuario.GetUsuario();
-    return Ok(data);
-  }
-
-  [HttpPost]
-  public async Task<IActionResult> GetRegister(Register user){
-      var data = await usuario.CreateUser(user);
-      return Ok(data);
-  }
-
-  [HttpPost]
-  [Route("login")]
-  public dynamic IniciarSesion([FromBody] Object optdata)
-  {
-
-    var data = JsonConvert.DeserializeObject<dynamic>(optdata.ToString());
-    string user = data.name.ToString();
-    string password = data.password.ToString();
-    Usuario usuarioApi = usuario.GetByUser(user, password);
-    if (usuarioApi == null)
+    public UsuarioController(IUsuario<Usuario> _usuario, IConfiguration configuration)
     {
-      return new
-      {
-        success = false,
-        message = "Credenciales icorrectas",
-        result = ""
-      };
+        this.usuario = _usuario;
+        this._configuration = configuration;
     }
 
-    var jwt = _configuration.GetSection("Jwt").Get<Jwt>();
-    var claims = new[]{
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var data = await usuario.GetUsuario();
+        return Ok(data);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> GetRegister(Register user)
+    {
+        var data = await usuario.CreateUser(user);
+        return Ok(data);
+    }
+
+    [HttpPost]
+    [Route("login")]
+    public dynamic login([FromBody] LoginUser user)
+    {
+        Usuario data = usuario.GetByUser(user.name, user.password);
+        if (data == null)
+        {
+            return new { success = false, message = "Usuario no encontrado" };
+        }
+        return new { success = true, message = "Usuario encontrado" , data};
+    }
+
+    [HttpPost]
+    [Route("token")]
+    public dynamic IniciarSesion([FromBody] Object optdata)
+    {
+        var data = JsonConvert.DeserializeObject<dynamic>(optdata.ToString());
+        string user = data.name.ToString();
+        string password = data.password.ToString();
+        Usuario usuarioApi = usuario.GetByUser(user, password);
+        if (usuarioApi == null)
+        {
+            return new
+            {
+                success = false,
+                message = "Credenciales icorrectas",
+                result = ""
+            };
+        }
+
+        var jwt = _configuration.GetSection("Jwt").Get<Jwt>();
+        var claims = new[]
+        {
             new Claim(JwtRegisteredClaimNames.Sub, jwt.Subject),
             new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
             new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
@@ -62,21 +75,21 @@ public class UsuarioController : Controller
             new Claim("name", usuarioApi.name),
         };
 
-    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt.Key));
-    var singIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-    var token = new JwtSecurityToken(
-        jwt.Issuer,
-        jwt.Audience,
-        claims,
-        expires: DateTime.Now.AddHours(3),
-        signingCredentials: singIn
-    );
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt.Key));
+        var singIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        var token = new JwtSecurityToken(
+            jwt.Issuer,
+            jwt.Audience,
+            claims,
+            expires: DateTime.Now.AddHours(3),
+            signingCredentials: singIn
+        );
 
-    return new
-    {
-      success = true,
-      messsage = "Exito",
-      result = new JwtSecurityTokenHandler().WriteToken(token)
-    };
-  }
+        return new
+        {
+            success = true,
+            messsage = "Exito",
+            result = new JwtSecurityTokenHandler().WriteToken(token)
+        };
+    }
 }
