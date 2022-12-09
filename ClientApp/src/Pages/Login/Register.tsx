08348d3d2d4f920d78512659a31dd037f0a32dbe
CoreDX1 <chrismaquis@gmail.com>
2022-12-09 20:23:30 -0300

    Validaciones para register

--- ClientApp/src/Pages/Login/Register.tsx
+++ ClientApp/src/Pages/Login/Register.tsx
@@ -1,8 +1,20 @@
-import { Form, Formik } from "formik";
+import { Field, Form, Formik } from "formik";
+ import * as Yup from 'yup';
 import "./_from.scss";
 import { useState } from "react";
 import { ListGet } from "../../Api/Menu";
 import { IEcommerse } from "../../Interface/Ecommerce";
+
+const SignupSchema = Yup.object().shape({
+  name: Yup.string()
+  .min(2, "Es muy corto el nombre")
+  .max(20, "Es muy largo el nombre")
+  .required("Required"),
+  password: Yup.string()
+  .min(5, "Es muy corto la contraseña")
+  .max(20, "Es muy larga la contraseña")
+  .required("Required"),
+})
 
 export const Register = (): JSX.Element => {
   const [register, setRegister] = useState<IEcommerse["register"]>();
@@ -11,6 +23,7 @@ export const Register = (): JSX.Element => {
     <div>
       <Formik
         initialValues={{ name: "", password: "" }}
+        validationSchema={SignupSchema}
         onSubmit={async (value) => {
           try {
             const response = await ListGet.sesion.register(value);
@@ -23,10 +36,10 @@ export const Register = (): JSX.Element => {
           <Form onSubmit={handleSubmit}>
             {register?.success ? <div>{register.message}</div> : <div>{register?.message}</div>}
             <label>Nombre</label>
-            <input type="text" name="name" onChange={handleChange} />
+            <Field type="text" name="name" onChange={handleChange} />
             {errors.name && touched.name ? <div>{errors.name}</div> : null}
             <label>Contraseña</label>
-            <input type="password" name="password" onChange={handleChange} />
+            <Field type="password" name="password" onChange={handleChange} />
             {errors.password && touched.password ? (
               <div>{errors.password}</div>
             ) : null}
