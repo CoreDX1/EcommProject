50f40c1e881ce30fa4a2527e234ef336b738683a
CoreDX1 <chrismaquis@gmail.com>
2023-01-05 21:54:57 -0300

    Remove Commenta

--- ClientService/Services/MenuDinamic.cs
+++ ClientService/Services/MenuDinamic.cs
@@ -41,32 +41,4 @@ public class MenuDinamic : IMenuDinamic
     List<Menu> menu = JsonConvert.DeserializeObject<List<Menu>>(json);
     return menu;
   }
-
-
-  // Iterar sobre cada categoría
-  // foreach (Category c in category)
-  // {
-  //     // Crear diccionario para almacenar información de la categoría
-  //     Dictionary<string, object> category_info = new Dictionary<string, object>();
-  //     category_info["id_category"] = c.id_category;
-  //     category_info["name"] = c.name;
-  //     category_info["submenu"] = new List<string>();
-
-  //     // Buscar subcategorías correspondientes a esta categoría
-  //     foreach (SubCategory subcategory in subCategories)
-  //     {
-  //         if (subcategory.id_category == c.id_category)
-  //         {
-  //             // Añadir subcategoría a la lista de subcategorías de esta categoría
-  //             ((List<string>)category_info["submenu"]).Add(subcategory.name);
-  //         }
-  //     }
-
-  //     // Añadir diccionario con información de la categoría a la lista de categorías
-  //     categories.Add(category_info);
-  // }
-
-  // Devolver lista de categorías en formato JSON
-  //     return linqDate;
-  // }
 }
