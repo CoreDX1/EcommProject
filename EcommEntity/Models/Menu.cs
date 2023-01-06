3159354c5adc20f704c2023607abd5ae069ad020
CoreDX1 <chrismaquis@gmail.com>
2023-01-05 22:11:10 -0300

    Chagen SubCategoru to String

--- EcommEntity/Models/Menu.cs
+++ EcommEntity/Models/Menu.cs
@@ -3,5 +3,5 @@ namespace EcommEntity.Models;
   public class Menu{
     public int id_category {get; set;}
     public string name {get; set;}
-    public List<SubCategory> submenu {get; set;}
+    public List<string> submenu {get; set;}
   }
